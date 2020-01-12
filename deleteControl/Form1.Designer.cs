8159ab581daebaa347e2c27645af92bced3fd991
Abdelilah77 <Abdelilah77@users.noreply.github.com>
2020-01-12 17:50:43 +0100

    first commit

---
+++ deleteControl/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace deleteControl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Supp = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Supprime_1 = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Supp,
+            this.Supprime_1});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 231);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(800, 219);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Supp
+            // 
+            this.Supp.HeaderText = "Supprime";
+            this.Supp.Name = "Supp";
+            // 
+            // Supprime_1
+            // 
+            this.Supprime_1.HeaderText = "Supprime";
+            this.Supprime_1.Name = "Supprime_1";
+            this.Supprime_1.Text = "Supprime_1";
+            this.Supprime_1.UseColumnTextForButtonValue = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(23, 100);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 53);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Supprime";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(157, 119);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(71, 17);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Check All";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Supp;
+        private System.Windows.Forms.DataGridViewButtonColumn Supprime_1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ deleteControl/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace deleteControl
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        DataClasses1DataContext dat = new DataClasses1DataContext();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Rempli();  dataGridView1.AllowUserToAddRows = false;
+        }
+        private void Rempli()
+        {
+          
+            var t = from emp in dat.users
+                    select emp;
+            dataGridView1.DataSource = t;
+        }
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int i = e.ColumnIndex;
+            if (i == 1)
+            {
+
+
+                string Dtgv = dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+                MessageBox.Show(Dtgv);
+                var a = dat.users.SingleOrDefault(delegate (user x)
+                {
+                    return x.empno.Equals(int.Parse(Dtgv));
+                });
+                dat.users.DeleteOnSubmit(a);
+                dat.SubmitChanges();
+                Rempli();
+
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                for (int i = 0; i < dataGridView1.RowCount; i++)
+                {
+                    dataGridView1.Rows[i].Cells[0].Value = true;
+                }
+            }
+            else
+            {
+                for (int i = 0; i < dataGridView1.RowCount; i++)
+                {
+                    dataGridView1.Rows[i].Cells[0].Value = false;
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+                if (Convert.ToBoolean(dataGridView1.Rows[i].Cells[0].Value))
+                {
+                    var deleted = dat.users.SingleOrDefault(delegate (user u)
+                    {
+                        return u.empno.Equals(int.Parse(dataGridView1.Rows[i].Cells[2].Value.ToString()));
+                    });
+                   
+                    dat.users.DeleteOnSubmit(deleted);
+                    dat.SubmitChanges();
+                }
+            }
+            Rempli();
+
+        }
+    }
+}
